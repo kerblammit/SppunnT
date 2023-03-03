ae00b347c78af298304b4efbd3c07f4415450f7f
HashbrownKazang <nickdieter52@gmail.com>
2023-03-03 01:47:25 -0500

    54V1N6 PR0GR355 CU2 1 R N00B

---
+++ contracts/gnosis-safe-contracts/transfer.sol
@@ -0,0 +1,21 @@
+
+{
+	"name": "transfer",
+	"type": "function",
+	"inputs": [
+		{
+			"type": "address",
+			"name": "_to"
+		},
+		{
+			"type": "uint256",
+			"name": "value"
+		}
+	],
+	"outputs": [
+		{
+			"type": "bool",
+			"name": ""
+		}
+	]
+}
